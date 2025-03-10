1f5a688c6fbe7eaafe67ad4362987262e9bac96c
mona <mona.halleem@gmail.com>
2025-03-10 20:43:01 +0200

    creating a messanger like screen in dark mode

--- lib/lab4/widgets/chat_item.dart
+++ lib/lab4/widgets/chat_item.dart
@@ -21,7 +21,7 @@ class ChatItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: index.isEven ? Colors.grey[800] : null,
+        color: index.isEven ? Color(0xff111111) : null,
         borderRadius: BorderRadius.circular(10),
       ),
       child: ListTile(
